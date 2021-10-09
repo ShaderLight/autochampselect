3d47b76445c3477df334a54dc82a4be3fcb7cd3f
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2021-10-09 19:42:49 +0200

    More input

--- modules/input.py
+++ modules/input.py
@@ -1,7 +1,14 @@
-from pyautogui import locateCenterOnScreen, click, ImageNotFoundException
+from pyautogui import locateCenterOnScreen, click, ImageNotFoundException, getWindowsWithTitle, write
+import pyautogui
 from .isrunning import isrunning
 import logging
 from time import sleep
+
+SELECT_X_RATIO = 309/1024
+SELECT_Y_RATIO = 132/576
+
+SEARCH_X_RATIO = 615/1024
+SEARCH_Y_RATIO = 83/576
 
 def pre_match_loop(window):
     while isrunning('LeagueClientUx.exe'):
@@ -29,5 +36,27 @@ def accept_match():
     click(acceptxy[0], acceptxy[1])
     return 0
 
-def searchbox_input(text):
+def searchbox_input(win, text):
+    champ_x = win.size.width * SELECT_X_RATIO
+    champ_y = win.size.height * SELECT_Y_RATIO
+
+    win.clickRel(x=champ_x, y=champ_y)
+    write(text, interval=0.1)
+
+    return 0
+
+def getlolwindow():
+    win = getWindowsWithTitle('League of Legends')[0]
+    logging.debug('LoL window: {}'.format(win.size))
+    return win
+
+def click_pickban():
     pass
+
+def selectchamp(win):
+    champ_x = win.size.width * SELECT_X_RATIO
+    champ_y = win.size.height * SELECT_Y_RATIO
+
+    win.clickRel(x=champ_x, y=champ_y)
+
+    return 0
